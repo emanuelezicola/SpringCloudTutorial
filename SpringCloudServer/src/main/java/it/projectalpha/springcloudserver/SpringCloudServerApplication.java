6290f0c7b453cdd4de596d5971a5203881787eb2
Emanuele Zicola <emanuelezicola95@gmail.com>
2022-07-28 00:09:59 +0200

    Creato server spring cloud da capo funzionante

---
+++ SpringCloudServer/src/main/java/it/projectalpha/springcloudserver/SpringCloudServerApplication.java
@@ -0,0 +1,15 @@
+package it.projectalpha.springcloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerApplication.class, args);
+    }
+
+}
